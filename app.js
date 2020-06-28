f5eab46272005a33bfee5a978da1d9a039917f3a
rashi <rashi.jaiswal@siesgst.ac.in>
2020-06-28 20:20:26 +0530

    heroku port

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static('project'));
 
-app.listen(3000, function() {
+app.listen(port, function() {
     console.log('Project is running on localhost:3000');
 });

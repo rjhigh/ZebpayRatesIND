fa24a8a8f6776150382da425caf96378c00ad362
rashi <rashi.jaiswal@siesgst.ac.in>
2020-06-29 09:52:06 +0530

    add js build

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/',express.static('project'));
+app.use(express.static('project'));
 
 app.listen(port, function() {
     console.log('Project is running on localhost:3000');
